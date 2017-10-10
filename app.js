57e7b4f19ed9199df2841557d25de73fbe4a635d
ed.jaimerosales <esteban.jaime.rosales1998@gmail.com>
2017-10-10 14:07:50 -0500

    se sagrego el puerto

--- app.js
+++ app.js
@@ -4,13 +4,14 @@
 //var nick="Saniel";
 //console.log(nombre, 'es',nick);
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+var port=process.env.PORT ||3000;
 
 app.get('/', function (req, res) {
   res.send('Esteban')
-})
+});
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!')
-})
+});
