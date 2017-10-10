e9e7beed0ad73734df3926e82c6729881e015692
ed.jaimerosales <esteban.jaime.rosales1998@gmail.com>
2017-10-10 14:02:32 -0500

    Ya existe la logica

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-console.log('Adios Mundo' );
+//console.log('Adios Mundo' );
 
-var nombre="esteban";
-var nick="Saniel";
-console.log(nombre, 'es',nick);
+//var nombre="esteban";
+//var nick="Saniel";
+//console.log(nombre, 'es',nick);
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Esteban')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
